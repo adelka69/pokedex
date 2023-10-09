110ed5288867578048ca17101e2877a4bfbcf5b5
ADEL KARA <karabesch@gmail.com>
2023-10-09 22:56:56 +0200

    chalenge reactBasic 03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,27 @@
 const PokemonCard = () => {
+  const pokemon = pokemonList[0];
+
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name || "Nom inconnu"} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name || "Nom inconnu"}</figcaption>
     </figure>
   );
 };
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 export default PokemonCard;
