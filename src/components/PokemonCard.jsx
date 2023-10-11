afcf0d7d8769ebbca7f93259279e9438fb22deba
ADEL KARA <karabesch@gmail.com>
2023-10-11 11:16:14 +0200

    chalenge reactBasic 05

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,4 +1,11 @@
+import PropTypes from "prop-types";
 const PokemonCard = ({ pokemon }) => {
+  PokemonCard.prototype = {
+    pokemon: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      imgSrc: PropTypes.string,
+    }),
+  };
   return (
     <figure>
       {pokemon.imgSrc ? (
